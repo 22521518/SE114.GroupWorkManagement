c62805436005a25a97b1e1fa4559ddb8dd9ea4af
22521518 <22521518@gm.uit.edu.vn>
2024-05-20 14:58:39 +0700

    remove sdk firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     id("com.google.dagger.hilt.android")
 
     kotlin("plugin.serialization")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -66,9 +65,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.firebase:firebase-auth:23.0.0")
-    implementation("com.google.firebase:firebase-firestore:25.0.0")
-    implementation("com.google.firebase:firebase-storage:21.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
